262f54c89cec44ee1388b249afa084ac5636c112
savayer <sava0094@gmail.com>
2020-01-29 23:54:34 +0800

    fix animating line under View project, removed img, added svg and animate top line

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -5,3 +5,7 @@ import "%modules%/blog/blog";
 import "%modules%/footer/footer"
 import './blur'
 import './modal'
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.add('loaded')
+})
