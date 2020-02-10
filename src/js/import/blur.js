3b603966916856b7c8ead2cdb31db3c1ce029a99
savayer <sava0094@gmail.com>
2020-02-10 14:02:34 +0800

    fixed footer, added mobile logos of clients to process page

--- src/js/import/blur.js
+++ src/js/import/blur.js
@@ -1,5 +1,6 @@
-const blurOnHover = (selector, hideOnMobile = false, cb) => {
-    if (document.documentElement.clientWidth <= 750 && hideOnMobile) {
+const blurOnHover = (selector, cb, execute = false) => {
+    let blur = execute ? 'empty' : 'light_blurred'
+    if (!execute && document.documentElement.clientWidth <= 750) {
         return
     }
     const items = document.querySelectorAll(selector)
@@ -25,19 +26,19 @@ const blurOnHover = (selector, hideOnMobile = false, cb) => {
                 item.classList.add('hover_js')
             }
             if (!item.classList.contains('hover')) {
-                item.classList.add('light_blurred')
+                item.classList.add(blur)
             }
         })
     }
     const unBlurAll = () => {
         document.querySelectorAll(selector).forEach(item => {
-            item.classList.remove('light_blurred', 'hover_js')
+            item.classList.remove(blur, 'hover_js')
         })
     }
 }
 
-blurOnHover('.services__item', true)
-blurOnHover('.process__item', true, function() {
+blurOnHover('.services__item')
+blurOnHover('.process__item', function() {
     document.querySelector('.process__wrapper').addEventListener('mouseleave', function() {
         this.closest('.process').classList.remove('loaded')
         setTimeout(() => {
@@ -45,5 +46,5 @@ blurOnHover('.process__item', true, function() {
         }, 0)
     })    
 })
-blurOnHover('.post__wrapper', true)
-blurOnHover('.logos__image')
+blurOnHover('.post__wrapper')
+blurOnHover('.logos__image', null, true)
