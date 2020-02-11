86923d1c9f6648e94d5dc3e4450e214766d9bd60
savayer <sava0094@gmail.com>
2020-02-11 22:31:43 +0800

    added screenshots of works to first three items in web category

--- src/blocks/modules/main/main.js
+++ src/blocks/modules/main/main.js
@@ -7,10 +7,10 @@ new Vue({
     data: {
         projectName: 'Test project',
         projectDescription: 'Full Branding Model',
-        showGoToSite: true,
         viewProject: false,
         works: [],
         filter: 'w-web',
+        projectMedia: null,
         categories: {
             'w-web': 'Web',
             'w-saas': 'Ui/Ux, Saas',
@@ -40,6 +40,27 @@ new Vue({
         preloadImage(url) {
             const img = new Image()
             img.src = url
+        },
+        popupProjectToggle(currentWork) {
+            document.body.classList.toggle('overflow-hidden')
+            document.querySelector('.overlay-project').classList.toggle('active')
+            if (currentWork) {
+                this.projectName = currentWork.name
+                this.projectDescription = currentWork.description
+                this.projectMedia = currentWork.images
+            }
+
+            this.viewProject = !this.viewProject
+            /* const scroll = document.documentElement.scrollTop
+            setTimeout(() => {
+                document.documentElement.scrollTop = scroll
+            }, 0) */
+        },
+        prevProject() {
+
+        },
+        nextProject() {
+
         }
     },
     mounted() {
@@ -56,15 +77,33 @@ new Vue({
             }
             const url = `../../../img/works/${imageIterator}.png`
             this.preloadImage(url)
-            
+
             this.works.push({
                 id: i+1,
                 name: `Test project ${i+1}`,
+                description: '',
                 image: url,
-                category: categories[categoryIterator]
-            })            
+                category: categories[categoryIterator],
+                images: null
+            })
             categoryIterator++
             imageIterator++
         }
+        this.works[10].images = [
+            '../../../img/works_view/2.png',
+            '../../../img/works_view/3.png',
+            '../../../img/works_view/4.png',
+            '../../../img/works_view/5.png',
+        ]
+        this.works[20].images = [
+            '../../../img/works_view/6.png',
+            '../../../img/works_view/7.png',
+            '../../../img/works_view/8.png',
+            '../../../img/works_view/9.png',
+            '../../../img/works_view/10.png',
+        ]
+        this.works[0].images = [
+            '../../../img/works_view/1.png',
+        ]
     }
 })
