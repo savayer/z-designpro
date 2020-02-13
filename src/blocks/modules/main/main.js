b97f144c56a8d4cf846a8e5ea14d14b9b65e831d
savayer <sava0094@gmail.com>
2020-02-13 15:36:39 +0800

    (wip) creating mobile view, load all works in poup window, implemented closing the modal by click back button on a smartphone

--- src/blocks/modules/main/main.js
+++ src/blocks/modules/main/main.js
@@ -5,136 +5,266 @@ import VueLazyload from 'vue-lazyload'
 Vue.use(VueMasonry)
 Vue.use(VueLazyload)
 
-const app = new Vue({
-    el: '#app',    
-    data: {
-        projectName: 'Test project',
-        projectDescription: 'Full Branding Model',
-        viewProject: false,
-        works: [],
-        filter: 'w-web',
-        projectMedia: null,
-        hiddenBody: false,
-        categories: {
-            'w-web': 'Web',
-            'w-saas': 'Ui/Ux, Saas',
-            'w-mobile': 'Mobile',
-            'w-wireframes': 'Wireframes',
-            'w-logo': 'Logo',
-            'w-tablet': 'Apps',
-            'w-print': 'Print',
-            'w-branding': 'Branding',
-            'w-presentations': 'Presentations',
+if (document.documentElement.clientWidth > 500) {
+    const app = new Vue({
+        el: '#app',    
+        data: {
+            projectName: 'Test project',
+            projectDescription: 'Full Branding Model',
+            viewProject: false,
+            works: [],
+            filter: 'w-web',
+            projectMedia: null,
+            projectMediaAll: [],
+            hiddenBody: false,
+            categories: {
+                'w-web': 'Web',
+                'w-saas': 'Ui/Ux, Saas',
+                'w-mobile': 'Mobile',
+                'w-wireframes': 'Wireframes',
+                'w-logo': 'Logo',
+                'w-tablet': 'Apps',
+                'w-print': 'Print',
+                'w-branding': 'Branding',
+                'w-presentations': 'Presentations',
+            },
+            currentIndexWork: null,
+            disabledPrevArrow: false,
+            disabledNextArrow: false,
         },
-        currentIndexWork: null,
-        disabledPrevArrow: false,
-        disabledNextArrow: false,
-    },
-    computed: {
-        filtered() {
-            return this.works.filter(work => work.category === this.filter)
-        }
-    },
-    methods: {
-        setActiveCategory(category) {
-            this.filter = category
-            const grid = document.querySelector('.grid_works')
-            grid.classList.add('animate-items')
-            setTimeout(() => {
-                grid.classList.remove('animate-items')
-            }, 300)
+        computed: {
+            filtered() {
+                return this.works.filter(work => work.category === this.filter)
+            }
         },
-        preloadImage(url) {
-            const img = new Image()
-            img.src = url
-        },
-        checkDisabledArrows() {
-            this.disabledPrevArrow = !this.filtered[this.currentIndexWork-1] ? true : false
-            this.disabledNextArrow = !this.filtered[this.currentIndexWork+1] ? true : false
-        },
-        switchToChosenWork() {
-            this.projectMedia = null
-            this.checkDisabledArrows()
-            const currentWork = this.filtered[this.currentIndexWork]
-            if (currentWork) {
-                this.projectName = currentWork.name
-                this.projectDescription = currentWork.description                
-                this.hiddenBody = true
+        methods: {
+            setActiveCategory(category) {
+                this.filter = category
+                const grid = document.querySelector('.grid_works')
+                grid.classList.add('animate-items')
                 setTimeout(() => {
-                    this.projectMedia = currentWork.images
-                    this.hiddenBody = false
-                }, 100)
-                setTimeout(() => {
-                    document.querySelector('.work').scrollTop = 0
-                }, 0)
-            } else {
+                    grid.classList.remove('animate-items')
+                }, 300)
+            },
+            preloadImage(url) {
+                const img = new Image()
+                img.src = url
+            },
+            checkDisabledArrows() {
+                this.disabledPrevArrow = !this.filtered[this.currentIndexWork-1] ? true : false
+                this.disabledNextArrow = !this.filtered[this.currentIndexWork+1] ? true : false
+            },
+            switchToChosenWork() {
                 this.projectMedia = null
+                this.checkDisabledArrows()
+                const currentWork = this.filtered[this.currentIndexWork]
+                if (currentWork) {
+                    this.projectName = currentWork.name
+                    this.projectDescription = currentWork.description                
+                    this.hiddenBody = true
+                    setTimeout(() => {
+                        this.projectMedia = currentWork.media
+                        this.hiddenBody = false
+                    }, 100)
+                    setTimeout(() => {
+                        document.querySelector('.work').scrollTop = 0
+                    }, 0)
+                } else {
+                    this.projectMedia = null
+                }
+            },
+            popupProjectToggle(index) {
+                this.currentIndexWork = index
+                this.switchToChosenWork()
+                document.body.classList.toggle('overflow-hidden')
+                document.querySelector('.overlay-project').classList.toggle('active')
+                document.querySelector('.svg_arrow__chevron--right').classList.add('shaking-to-right')
+                this.viewProject = !this.viewProject
+            },
+            prevProject() {
+                if (this.disabledPrevArrow) return;
+                this.currentIndexWork--
+                this.switchToChosenWork()
+            },
+            nextProject() {
+                if (this.disabledNextArrow) return;
+                this.currentIndexWork++
+                this.switchToChosenWork()
             }
         },
-        popupProjectToggle(index) {
-            this.currentIndexWork = index
-            this.switchToChosenWork()
-            document.body.classList.toggle('overflow-hidden')
-            document.querySelector('.overlay-project').classList.toggle('active')
-            document.querySelector('.svg_arrow__chevron--right').classList.add('shaking-to-right')
-            this.viewProject = !this.viewProject
-        },
-        prevProject() {
-            if (this.disabledPrevArrow) return;
-            this.currentIndexWork--
-            this.switchToChosenWork()
-        },
-        nextProject() {
-            if (this.disabledNextArrow) return;
-            this.currentIndexWork++
-            this.switchToChosenWork()
+        mounted() {
+            const categories = Object.keys(this.categories)
+            let categoryIterator = 0
+            let imageIterator = 1
+    
+            for (let i = 0; i < 100; i++) {
+                if (imageIterator === 19) {
+                    imageIterator = 1
+                }
+                if (categoryIterator === 10) {
+                    categoryIterator = 0
+                }
+                const url = `../../../img/works/${imageIterator}.png`
+                this.preloadImage(url)
+    
+                this.works.push({
+                    id: i+1,
+                    name: `Test project ${i+1}`,
+                    description: '',
+                    slug: `test-project-${i+1}`,
+                    image: url,
+                    category: categories[categoryIterator],
+                    media: null
+                })
+                categoryIterator++
+                imageIterator++
+            }
+            this.works[10].media = [
+                '../../../img/works_view/2.png',
+                '../../../img/works_view/3.png',
+                '../../../img/works_view/4.png',
+                '../../../img/works_view/5.png',
+            ]
+            this.works[20].media = [
+                '../../../img/works_view/6.png',
+                '../../../img/works_view/7.png',
+                '../../../img/works_view/8.png',
+                '../../../img/works_view/9.png',
+                '../../../img/works_view/10.png',
+            ]
+            this.works[20].name = 'Galim project'
+            this.works[20].slug = 'galim-project'
+            this.works[20].description = 'Full branding model'
+            this.works[0].media = [
+                '../../../img/works_view/1.png',
+                {
+                    type: 'video',
+                    src: 'https://www.youtube.com/embed/DyDfgMOUjCI'
+                }
+            ]
         }
-    },
-    mounted() {
-        const categories = Object.keys(this.categories)
-        let categoryIterator = 0
-        let imageIterator = 1
+    })
+}
 
-        for (let i = 0; i < 100; i++) {
-            if (imageIterator === 19) {
-                imageIterator = 1
+if (document.documentElement.clientWidth <= 500) {
+    document.querySelector('.work__header').classList.remove('work__header--desk_view')
+    const app = new Vue({
+        el: '#app',    
+        data: {
+            projectName: 'Test project',
+            projectDescription: 'Full Branding Model',
+            viewProject: false,
+            works: [],
+            filter: 'w-web',
+            projectMedia: null,
+            projectMediaAll: [],
+            hiddenBody: false,
+            categories: {
+                'w-web': 'Web',
+                'w-saas': 'Ui/Ux, Saas',
+                'w-mobile': 'Mobile',
+                'w-wireframes': 'Wireframes',
+                'w-logo': 'Logo',
+                'w-tablet': 'Apps',
+                'w-print': 'Print',
+                'w-branding': 'Branding',
+                'w-presentations': 'Presentations',
+            },
+            currentIndexWork: null,
+            disabledPrevArrow: false,
+            disabledNextArrow: false,
+            route: ''
+        },
+        computed: {
+            filtered() {
+                return this.works.filter(work => work.category === this.filter)
             }
-            if (categoryIterator === 10) {
-                categoryIterator = 0
+        },
+        methods: {
+            setActiveCategory(category) {
+                this.filter = category
+                const grid = document.querySelector('.grid_works')
+                grid.classList.add('animate-items')
+                setTimeout(() => {
+                    grid.classList.remove('animate-items')
+                }, 300)
+            },
+            preloadImage(url) {
+                const img = new Image()
+                img.src = url
+            },
+            loadWorks() {
+                this.filtered.forEach(work => {
+                    this.projectMediaAll.push(work.media)
+                })
+                window.history.pushState(null, null, '#projects')
+                window.dispatchEvent(new Event('popstate'))
+            },
+            popupProjectToggle(index) {
+                this.loadWorks()
+                document.querySelector('.overlay-project').classList.toggle('active')
+                this.viewProject = !this.viewProject
             }
-            const url = `../../../img/works/${imageIterator}.png`
-            this.preloadImage(url)
-
-            this.works.push({
-                id: i+1,
-                name: `Test project ${i+1}`,
-                description: '',
-                slug: `test-project-${i+1}`,
-                image: url,
-                category: categories[categoryIterator],
-                images: null
-            })
-            categoryIterator++
-            imageIterator++
+        },
+        created() {
+            window.onpopstate = e => {
+                console.log(e, document.location.hash)
+                if (this.viewProject && e.isTrusted) {
+                    document.querySelector('.overlay-project').classList.remove('active')
+                    this.viewProject = false
+                }
+            }
+        },
+        mounted() {
+            const categories = Object.keys(this.categories)
+            let categoryIterator = 0
+            let imageIterator = 1
+    
+            for (let i = 0; i < 100; i++) {
+                if (imageIterator === 19) {
+                    imageIterator = 1
+                }
+                if (categoryIterator === 10) {
+                    categoryIterator = 0
+                }
+                const url = `../../../img/works/${imageIterator}.png`
+                this.preloadImage(url)
+    
+                this.works.push({
+                    id: i+1,
+                    name: `Test project ${i+1}`,
+                    description: '',
+                    slug: `test-project-${i+1}`,
+                    image: url,
+                    category: categories[categoryIterator],
+                    images: null
+                })
+                categoryIterator++
+                imageIterator++
+            }
+            this.works[10].media = [
+                '../../../img/works_view/2.png',
+                '../../../img/works_view/3.png',
+                '../../../img/works_view/4.png',
+                '../../../img/works_view/5.png',
+            ]
+            this.works[20].media = [
+                '../../../img/works_view/6.png',
+                '../../../img/works_view/7.png',
+                '../../../img/works_view/8.png',
+                '../../../img/works_view/9.png',
+                '../../../img/works_view/10.png',
+            ]
+            this.works[20].name = 'Galim project'
+            this.works[20].slug = 'galim-project'
+            this.works[20].description = 'Full branding model'
+            this.works[0].media = [
+                '../../../img/works_view/1.png',
+                {
+                    type: 'video',
+                    src: 'https://www.youtube.com/embed/DyDfgMOUjCI'
+                }
+            ]
         }
-        this.works[10].images = [
-            '../../../img/works_view/2.png',
-            '../../../img/works_view/3.png',
-            '../../../img/works_view/4.png',
-            '../../../img/works_view/5.png',
-        ]
-        this.works[20].images = [
-            '../../../img/works_view/6.png',
-            '../../../img/works_view/7.png',
-            '../../../img/works_view/8.png',
-            '../../../img/works_view/9.png',
-            '../../../img/works_view/10.png',
-        ]
-        this.works[20].name = 'Galim project'
-        this.works[20].slug = 'galim-project'
-        this.works[20].description = 'Full branding model'
-        this.works[0].images = [
-            '../../../img/works_view/1.png',
-        ]
-    }
-})
+    })
+}
