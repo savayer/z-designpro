f189942fee62fc5b8f6756b4d0b45817292b174b
savayer <sava0094@gmail.com>
2020-02-18 16:42:42 +0800

    fixed blur animation

--- src/js/import/index/index.js
+++ src/js/import/index/index.js
@@ -1,6 +1,6 @@
+import "./mainAnimate/animate";
 import "%modules%/nav/nav";
 import "%modules%/menu/menu";
 import "%modules%/main/main";
 import "%modules%/footer/footer";
 import "../modal";
-import "./mainAnimate/animate";
