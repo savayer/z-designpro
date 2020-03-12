2bee769b429796924dce7b522d4865349903c231
savayer <sava0094@gmail.com>
2020-03-13 01:17:12 +0800

    add animation on toggle menu by mouse out

--- src/blocks/modules/nav/nav.js
+++ src/blocks/modules/nav/nav.js
@@ -2,6 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const menu = document.getElementById('toggle_menu')
     menu.classList.add('load')
 
+    menu.addEventListener('mouseover', e => {
+        menu.classList.add('mouseover')
+        menu.classList.remove('mouseout')
+    })
+
+    menu.addEventListener('mouseout', e => {
+        menu.classList.add('mouseout')
+        menu.classList.remove('mouseover')
+    })
+
     const navInner = document.querySelector('.nav--inner')
     const navHome = document.querySelector('.nav--home')
     const navWorks = document.querySelector('.nav_works')
